d3ad6229c52f5f97b76fdbfd282c9a4c10e1230f
fvnny-code <f.codecco.grando@gmail.com>
2023-03-10 16:08:19 +0100

    installing and creation of the router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,15 @@
-
-
 import React from 'react';
 
 import './App.css';
+import Router from './Router';
 
 export default function App() {
   return (
     <div className="App">
-      pouet
+      <main>
+        <Router />
+      </main>
+
     </div>
   );
 }
