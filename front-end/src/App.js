9bb13a5e9fe9fe7cd66974a342f3b8e469ed24bb
fvnny-code <f.codecco.grando@gmail.com>
2023-03-10 17:02:55 +0100

    General and header styling

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import Header from './layouts/Header';
+import Header from './layouts/Header/Header';
 
 
 import './App.css';
--- front-end/src/layouts/Header/Header.jsx
+++ front-end/src/layouts/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from '../assets/logo.png';
+import logo from '../../assets/logo.png';
+import './headerStyle.css'
 
 
 export default function Header() {
