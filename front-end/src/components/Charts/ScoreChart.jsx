5bbc1e853eb34887473d94118bb60747e56bd337
fvnny-code <f.codecco.grando@gmail.com>
2023-04-28 11:24:27 +0200

    display of ScoreChart = KO

--- front-end/src/components/Charts/ScoreChart.jsx
+++ front-end/src/components/Charts/ScoreChart.jsx
@@ -11,16 +11,44 @@ export function ScoreChart() {
     return <></>;
   }
 
+  const score = [
+    { value: context.score.todayScore || context.score },
+    { value: 1 - context.score.todayScore || context.score },
+  ];
+
+
   return (
     <div className="ScoreChartContainer">
       <h2 className="ScoreChartTitle">Score</h2>
       <ResponsiveContainer width="100%" height="100%">
         <PieChart>
-          <Pie>
-            <Cell></Cell>
+          <Pie
+            data={score}
+            dataKey="value"
+            cx="50%"
+            cy="50%"
+            innerRadius={70}
+            outerRadius={85}
+            startAngle={90}
+          >
+            {score.map((entry, index) =>
+              index === 0 ? (
+                <Cell key={`cell-${index}`} cornerRadius={10} fill="#ff0000" />
+              ) : (
+                <Cell key={`cell-${entry}`} fill="#E60000" />
+              )
+            )}
           </Pie>
         </PieChart>
       </ResponsiveContainer>
+      <div className="score">
+        <p className="scorePercent">{score[0].value * 100}%</p>
+        <p className="scoreText">
+          de votre
+          <br />
+          objectif
+        </p>
+      </div>
     </div>
   );
 }
