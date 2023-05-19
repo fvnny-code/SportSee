7706773e6b0cb1c90f6120fe1e658673e2eded80
fvnny-code <f.codecco.grando@gmail.com>
2023-05-19 21:58:28 +0200

    code cleaning

--- front-end/src/utils/context.js
+++ front-end/src/utils/context.js
@@ -26,7 +26,6 @@ export const UserProvider = ({ children }) => {
             getUserScoreInfos(userId),
 
         ])
-        console.log(userInfos);
         setInfos((state) => {
             state.userInfos = userInfos
 
